f0a6450398e40d33f565b984a95d4aec29c77653
cfarrell23 <45257855+cfarrell23@users.noreply.github.com>
2023-02-03 11:19:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import zipfile
+
+def zipFiles(path):
+    projectPath = os.path.abspath()
+    for folder in os.listdir(projectPath):
+        zipf = zipfile.ZipFile('{0}.zip'.format(os.path.join(projectPath, folder)), 'w', zipfile.ZIP_DEFLATED)
+    for root, dirs, files in os.walk(os.path.join(projectPath, folder)):
+        for filename in files:
+            zipf.write(os.path.abspath(os.path.join(root, filename)), arcname=filename)
+    zipf.close()
+
+if __name__== "__main__":
+    zipFiles("test-files")
