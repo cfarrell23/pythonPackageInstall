2ebcb04bf174732a288629a9f36fab087ff4ac0c
cfarrell23 <45257855+cfarrell23@users.noreply.github.com>
2023-02-03 11:55:35 +0000

    Updated zipFiles function

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import zipfile
 
 def zipFiles(path):
     projectPath = os.path.abspath(path)
-    print(projectPath)
+    print("Project path: " + projectPath)
     for folder in os.listdir(projectPath):
         zipf = zipfile.ZipFile('{0}.zip'.format(os.path.join(projectPath, folder)), 'w', zipfile.ZIP_DEFLATED)
-    for root, dirs, files in os.walk(os.path.join(projectPath, folder)):
-        for filename in files:
-            zipf.write(os.path.abspath(os.path.join(root, filename)), arcname=filename)
-    zipf.close()
-    return zipf
+        for root, dirs, files in os.walk(os.path.join(projectPath, folder)):
+            for filename in files:
+                zipf.write(os.path.abspath(os.path.join(root, filename)), arcname=filename)
+        zipf.close()
 
 if __name__== "__main__":
-    zipFiles("test-files")
+    zipf = zipFiles("test-files")
