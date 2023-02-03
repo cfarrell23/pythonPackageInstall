868c32075439895d78bb29fc2e3f4f0d59b3e79e
cfarrell23 <45257855+cfarrell23@users.noreply.github.com>
2023-02-03 11:47:11 +0000

    Fixed spelling mistake

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def zipFiles(path):
     projectPath = os.path.abspath(path)
     print(projectPath)
     for folder in os.listdir(projectPath):
-        zipf = zipfile.ZipFile('{0}.zip'.formats(os.path.join(projectPath, folder)), 'w', zipfile.ZIP_DEFLATED)
+        zipf = zipfile.ZipFile('{0}.zip'.format(os.path.join(projectPath, folder)), 'w', zipfile.ZIP_DEFLATED)
     for root, dirs, files in os.walk(os.path.join(projectPath, folder)):
         for filename in files:
             zipf.write(os.path.abspath(os.path.join(root, filename)), arcname=filename)
