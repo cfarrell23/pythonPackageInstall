8e9bf96d8484ff0223222de99d638e18201a522f
cfarrell23 <45257855+cfarrell23@users.noreply.github.com>
2023-02-03 11:57:39 +0000

    Added additional logs

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def zipFiles(path):
     projectPath = os.path.abspath(path)
     print("Project path: " + projectPath)
     for folder in os.listdir(projectPath):
+        print(folder)
         zipf = zipfile.ZipFile('{0}.zip'.format(os.path.join(projectPath, folder)), 'w', zipfile.ZIP_DEFLATED)
         for root, dirs, files in os.walk(os.path.join(projectPath, folder)):
             for filename in files:
