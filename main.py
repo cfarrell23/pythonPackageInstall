1965cdab048f232f91fa957cb5f25b8d25f71321
cfarrell23 <45257855+cfarrell23@users.noreply.github.com>
2023-02-03 13:08:27 +0000

    Added second test

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import zipfile
 
 def zipFiles(path):
     projectPath = os.path.abspath(path)
-    print("Project path: " + projectPath)
+    #print("Project path: " + projectPath)
     for folder in os.listdir(projectPath):
-        print(folder)
+        #print(folder)
         zipf = zipfile.ZipFile('{0}.zip'.format(os.path.join(projectPath, folder)), 'w', zipfile.ZIP_DEFLATED)
         for root, dirs, files in os.walk(os.path.join(projectPath, folder)):
             for filename in files:
@@ -14,3 +14,4 @@ def zipFiles(path):
 
 if __name__== "__main__":
     zipf = zipFiles("test-files")
+    zipfExternal = zipFiles("test-files2")
